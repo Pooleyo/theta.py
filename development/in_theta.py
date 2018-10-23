4a5bb444a26885e04f137dfd37c8a1944974d0e9
Ashley Poole <ajp560@york.ac.uk>
2018-10-23 15:35:53 +0100

    Intermediate commit: fixing g^2
    Trying to diagnose the problem with G^2 calculation. At the moment
    it looks like there's a mixup of definitions that's causing it
    to fall over. It might be best to rewrite the vector calculation
    part.

--- development/in_theta.py
+++ development/in_theta.py
@@ -1,28 +1,28 @@
 image_filename = ["3x4_pixel_value_1.tif"] # "3x4_pixel_value_1.tif" "s10260_PSL_forward_scatter_plate.tif" #"PSL_plate_4_s10268_BBXRD.tif" #"test_forward_scatter.tif"
 
 source_position = [[50.0,0.0,50.0]]  # In mm
-offset = [[0,11,270]]  # X offset (mm), Y offset (mm), rotation (degrees)
+offset = [[0,0,0]]  # X offset (mm), Y offset (mm), rotation (degrees)
 sample_normal = [[0.0, 0.0, 1.0]]
 
 
-normal = [[-23.5, 0.0, 12.3]]  # The normal to the plane of the image plate with units mm.
+normal = [[-40.0, 0.0, 40.0]]  # The normal to the plane of the image plate with units mm.
 
-x_scale = [56]  # In mm
-y_scale = [44]  # In mm
+x_scale = [20]  # In mm
+y_scale = [20]  # In mm
 
 view_x = [[0.0, 1.0, 0.00]]  # "normalised"
-view_y = [[0.46, 0.00, 0.89]]  # "normalised"
+view_y = [[0.71, 0.00, 0.71]]  # "normalised"
 
 
-wavelength = 1.851  # In Angstroms
-a_lattice = 3.3  # In Angstroms
+wavelength = 1.0  # In Angstroms
+a_lattice = 1.0 # In Angstroms
 
 num_subpixels_height = 1
 num_subpixels_width = 1
 
 correct_for_filter_attenuation = True
-filter_thickness = [10.0]  # [10.0, 6.0]
-filter_attenuation_length_at_90_deg = [34.1]  # [34.1, 109.7] # The attenuation length(s) of filter(s) used, in microns.
+filter_thickness = [0.0]  # [10.0, 6.0]
+filter_attenuation_length_at_90_deg = [100.0]  # [34.1, 109.7] # The attenuation length(s) of filter(s) used, in microns.
 # Enter a new list element for each filter; the order doesn't matter. Zn, at 9 keV, has attenuation length of 34.1
 # microns. Al, at 9 keV, has attenuation length of 109.7 microns.
 
@@ -30,8 +30,8 @@ correct_for_sample_attenuation = True
 
 correct_for_polarisation = True
 
-gsqr_limit = [[0.0, 9.5]]
-phi_limit = [[-60.0, 60.0]]
+gsqr_limit = [[0.0, 25.0]]
+phi_limit = [[-160.0, 160.0]]
 
 num_gsqr_bins = 200
 num_phi_bins = 100
