429985ed195f0b08de6c2f4ddc00fae85386962c
Ashley Poole <ajp560@york.ac.uk>
2018-10-29 12:18:10 +0000

    First commit after the roll back
    It looks like my work on saving previous G^2 values has been
    deleted. That's a shame... but should be quick to reimplement.

--- development/in_theta_2.py
+++ development/in_theta_2.py
@@ -1,4 +1,4 @@
-image_filename = ["PSL_plate_3_s10268_BBXRD.tif", "PSL_plate_2_s10268_BBXRD.tif", "PSL_plate_1_s10268_BBXRD.tif", "PSL_plate_4_s10268_BBXRD.tif",] # "3x4_pixel_value_1.tif" "s10260_PSL_forward_scatter_plate.tif" #"PSL_plate_4_s10268_BBXRD.tif" #"test_forward_scatter.tif"
+image_filename = ["PSL_plate_3_s10268_BBXRD.tif", "PSL_plate_2_s10268_BBXRD.tif", "PSL_plate_1_s10268_BBXRD.tif", "PSL_plate_4_s10268_BBXRD.tif"] # "3x4_pixel_value_1.tif" "s10260_PSL_forward_scatter_plate.tif" #"PSL_plate_4_s10268_BBXRD.tif" #"test_forward_scatter.tif"
 
 source_position = [[50.0, 0.0, 50.0], [50.0, 0.0, 50.0], [50.0, 0.0, 50.0], [50.0, 0.0, 50.0]]  # In mm
 offset = [[1, 11.7, 270], [0.5, 11, 270], [0, 11.7, 180], [0, 11.7, 270]]  # X offset (mm), Y offset (mm), rotation (degrees)
@@ -20,15 +20,15 @@ a_lattice = 3.3  # In Angstroms
 num_subpixels_height = 1
 num_subpixels_width = 1
 
-correct_for_filter_attenuation = True
+correct_for_filter_attenuation = False
 filter_thickness = [10.0, 10.0, 10.0, 10.0]  # [10.0, 6.0]
 filter_attenuation_length_at_90_deg = [34.1, 34.1, 34.1, 34.1]  # [34.1, 109.7] # The attenuation length(s) of filter(s) used, in microns.
 # Enter a new list element for each filter; the order doesn't matter. Zn, at 9 keV, has attenuation length of 34.1
 # microns. Al, at 9 keV, has attenuation length of 109.7 microns.
 
-correct_for_sample_attenuation = True
+correct_for_sample_attenuation = False
 
-correct_for_polarisation = True
+correct_for_polarisation = False
 
 gsqr_limit = [[3.0, 22.0], [3.0, 22.0], [3.0, 22.0], [3.0, 22.0]]
 phi_limit = [[-180.0, 180.0], [-180.0, 180.0], [-180.0, 180.0], [-180.0, 180.0]]
